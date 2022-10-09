4bb20acc4b4d27e878f9c2e04635eff4dfb7243d
Grishin-Nikolai-ifbs21 <kolyagrishin13@gmail.com>
2022-10-09 18:40:35 +0400

    Update Program.cs

--- task-6/Program.cs
+++ task-6/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите стороны треугольника\n");
 
-double checkSide()
+int checkSide()
 {
     Console.Write("Введите сторону треугольника: ");
 
-    double tmp;
-    if (!double.TryParse(Console.ReadLine(), out tmp))
+    int tmp;
+    if (!int.TryParse(Console.ReadLine(), out tmp))
     {
         Console.Write("\nОшибка! Было введено не число!");
         Console.ReadLine();
@@ -22,13 +22,13 @@ double checkSide()
     return tmp;
 }
 
-double a = checkSide();
-double b = checkSide();
-double c = checkSide();
+int a = checkSide();
+int b = checkSide();
+int c = checkSide();
 
 if (a + b > c && b + c > a && a + c > b)
 {
-    double p = (a + b + c) / 2;
+    double p = (double)(a + b + c) / 2;
   
     Console.Write($"\nПлощаль треугольника: {Math.Sqrt(p * (p - a) * (p - b) * (p - c))}");
 }
