ceacddc6375781a6372e61013b8b8e954225837c
Николай Гришин ИФБС-21 <115008137+Grishin-Nikolai-ifbs21@users.noreply.github.com>
2022-10-17 16:52:04 +0400

    Update Program.cs

--- task-7/Program.cs
+++ task-7/Program.cs
@@ -1,26 +1,31 @@
-﻿byte checkNubmer()
+byte? checkNubmer()
 {
     Console.Write("Введите число: ");
 
     byte tmp;
     if (!byte.TryParse(Console.ReadLine(), out tmp))
     {
-        Console.Write("\nОшибка! Введите число в диапазоне от 0 до 255!");
-        Console.ReadLine();
-        Environment.Exit(0);
+        return null;
     }
 
     return tmp;
 }
 
-int firstNumber = checkNubmer();
-int secondNumber = checkNubmer();
+byte? firstNumber = checkNubmer();
+byte? secondNumber = checkNubmer();
 
 Console.Write('\n');
 
-Console.Write($"AND: {firstNumber & secondNumber}\n");
-Console.Write($"OR: {firstNumber | secondNumber}\n");
-Console.Write($"XOR: {firstNumber ^ secondNumber}\n");
+if (firstNumber == null || secondNumber == null)
+{
+    Console.Write("\nОшибка! Введите число в диапазоне от 0 до 255!");
+}
+else
+{
+    Console.Write($"AND: {firstNumber & secondNumber}\n");
+    Console.Write($"OR: {firstNumber | secondNumber}\n");
+    Console.Write($"XOR: {firstNumber ^ secondNumber}\n");
+}
 
 
 Console.ReadLine();
